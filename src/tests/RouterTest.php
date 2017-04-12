d81b3cc7c52b2296f5cf599a5ccca791488b0b5b
Jagepard <dankorot@gmail.com>
2017-04-12 18:49:32 +0300

    Добавляет  тесты

--- src/tests/RouterTest.php
+++ src/tests/RouterTest.php
@@ -36,31 +36,132 @@ class RouterTest extends PHPUnit_Framework_TestCase
      */
     protected $app;
 
-    protected function setUp(): void
+    protected function setContainer()
     {
-        $_SERVER['REQUEST_URI']    = 'test/page';
-        $_SERVER['REQUEST_METHOD'] = 'GET';
+        Container::$app = null;
         $this->container           = Container::app();
         $this->container->setBinding(ContainerInterface::class, Container::$app);
         $this->container->set('annotation', 'Rudra\Annotations');
         $this->container->set('router', 'Rudra\Router', ['namespace' => 'stub\\', 'templateEngine' => 'twig']);
     }
 
-
-    public function testRouter(): void
+    public function testGet(): void
     {
-        $this->assertInstanceOf(Router::class, $this->container()->get('router'));
+        $_SERVER['REQUEST_URI']    = 'test/page';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $this->setContainer();
 
-        $this->container()->get('router')->set([
-                'name'        => 'main_page',
-                'pattern'     => '/test/page',
-                'http_method' => 'GET',
-                'controller'  => 'stub\\MainController',
-                'method'      => 'actionIndex'
+        $this->container()->get('router')->get([
+                'name'       => 'main_page',
+                'pattern'    => '/test/page',
+                'controller' => 'stub\\MainController::namespace',
+                'method'     => 'actionGet'
             ]
         );
 
-        $this->assertEquals(123, Container::$app->get('actionIndex'));
+        $this->assertEquals('GET', $this->container()->get('actionGet'));
+    }
+
+    public function testPost(): void
+    {
+        $_SERVER['REQUEST_URI']    = 'test/page?some=123';
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $this->setContainer();
+
+        $this->container()->get('router')->post([
+                'name'       => 'main_page',
+                'pattern'    => '/test/page',
+                'controller' => 'MainController',
+                'method'     => 'actionPost'
+            ]
+        );
+
+        $this->assertEquals('POST', $this->container()->get('actionPost'));
+    }
+
+    public function testPut(): void
+    {
+        $_SERVER['REQUEST_URI']    = 'test/page';
+        $_SERVER['REQUEST_METHOD'] = 'PUT';
+        $this->setContainer();
+
+        $this->container()->get('router')->put([
+                'name'       => 'main_page',
+                'pattern'    => '/test/page',
+                'controller' => 'MainController',
+                'method'     => 'actionPut'
+            ]
+        );
+
+        $this->assertEquals('PUT', $this->container()->get('actionPut'));
+    }
+
+    public function testPatch(): void
+    {
+        $_SERVER['REQUEST_URI']    = 'test/page';
+        $_SERVER['REQUEST_METHOD'] = 'PATCH';
+        $this->setContainer();
+
+        $this->container()->get('router')->patch([
+                'name'       => 'main_page',
+                'pattern'    => '/test/page',
+                'controller' => 'MainController',
+                'method'     => 'actionPatch'
+            ]
+        );
+
+        $this->assertEquals('PATCH', $this->container()->get('actionPatch'));
+    }
+
+    public function testDelete(): void
+    {
+        $_SERVER['REQUEST_URI']    = 'test/page';
+        $_SERVER['REQUEST_METHOD'] = 'DELETE';
+        $this->setContainer();
+
+        $this->container()->get('router')->delete([
+                'name'       => 'main_page',
+                'pattern'    => '/test/page',
+                'controller' => 'MainController',
+                'method'     => 'actionDelete'
+            ]
+        );
+
+        $this->assertEquals('DELETE', $this->container()->get('actionDelete'));
+    }
+
+    public function testAny(): void
+    {
+        $_SERVER['REQUEST_URI']    = 'test/page';
+        $_SERVER['REQUEST_METHOD'] = 'PATCH';
+        $this->setContainer();
+
+        $this->container()->get('router')->any([
+                'name'       => 'main_page',
+                'pattern'    => '/test/page',
+                'controller' => 'MainController',
+                'method'     => 'actionAny'
+            ]
+        );
+
+        $this->assertEquals('ANY', $this->container()->get('actionAny'));
+    }
+
+    public function testMatchFalse()
+    {
+        $_SERVER['REQUEST_URI']    = 'test/false';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $this->setContainer();
+
+        $this->container()->get('router')->get([
+                'name'       => 'main_page',
+                'pattern'    => '/test/page',
+                'controller' => 'MainController',
+                'method'     => 'actionGet'
+            ]
+        );
+
+        $this->assertEquals(false, $this->container()->get('router')->isToken());
     }
 
     /**
