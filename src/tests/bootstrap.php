df4a09eaa0983c5752a72bd71e9672879d19c292
Jagepard <dankorot@gmail.com>
2017-04-19 17:45:47 +0300

    Добавляет константу DEV

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+define('DEV', 'test');
+
 require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/tests/stub/MainController.php';
 require_once dirname(__DIR__) . '/tests/stub/Middleware.php';
