e46a9626309adeaf9546eb74310fe86f0ab7560b
Jagepard <dankorot@gmail.com>
2017-04-21 19:43:21 +0300

    Добавляет RouterMiddlewareTrait

---
+++ src/RouterMiddlewareTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Date: 21.04.17
+ * Time: 19:24
+ *
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @copyright Copyright (c) 2016, Korotkov Danila
+ * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
+ */
+
+namespace Rudra;
+
+/**
+ * Class RouterMiddlewareTrait
+ *
+ * @package Rudra
+ */
+trait RouterMiddlewareTrait
+{
+
+    /**
+     * @param $middleware
+     */
+    public function middleware($middleware): void
+    {
+        $this->container()->get('router')->handleMiddleware($middleware);
+    }
+}
