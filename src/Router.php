145cf66a30cc1eb9782f902f4cae3883720fd256
Jagepard <dankorot@gmail.com>
2017-04-25 13:32:12 +0300

    Проходит тесты

--- src/Router.php
+++ src/Router.php
@@ -18,8 +18,12 @@ namespace Rudra;
  *
  * @package Rudra
  */
-class Router //implements RouterInterface
+class Router implements RouterInterface
 {
+    /**
+     * @var RouterFacade
+     */
+    protected $routerFacade;
 
     use SetContainerTrait;
 
@@ -37,13 +41,13 @@ class Router //implements RouterInterface
         $this->templateEngine = $templateEngine;
         set_exception_handler([new RouterException(), 'handler']);
 
-        $this->routerFacade = new RouterFacade($this->container, new RequestMethod($this->container), new MatchMethod($this->container));
+        $this->routerFacade = new RouterFacade(
+            $this->container,
+            new RequestMethod($this->container),
+            new MatchMethod($this->container, new MatchRequest($this->container, $this))
+        );
     }
 
-    /**
-     * @var RouterFacade
-     */
-    protected $routerFacade;
 
     /**
      * @return RouterFacade
@@ -168,7 +172,7 @@ class Router //implements RouterInterface
                 break;
             case 'POST':
                 if ($this->container()->hasPost('_method')) {
-                    $route = array_merge($route, $this->setRequestMethod('REST'));
+                    $route = array_merge($route, $this->routerFacade()->requestMethod()->setRequestMethod('REST'));
                 } else {
                     $route['http_method'] = 'POST';
                     $route['method']      = 'create';
@@ -226,135 +230,11 @@ class Router //implements RouterInterface
 
     /**
      * @param array $route
-     *
-     * @return bool|void
-     */
-    protected function matchRequest(array $route)
-    {
-        if ($route['http_method'] == $this->container()->getServer('REQUEST_METHOD')) {
-
-            $requestUrl   = trim($this->container()->getServer('REQUEST_URI'), '/');
-            $requestArray = explode('/', $requestUrl);
-            list($params, $completeRequestArray) = $this->handlePattern($route, $requestArray);
-            list($requestString, $realRequestString) = $this->handleCompleteRequestArray($completeRequestArray);
-            $outRequestUrl = $this->getOutRequestUrl($requestUrl);
-
-            // Это нужно для обработки 404 ошибки
-            if (isset($requestString)) {
-                // Проверяем строку запроса на соответсвие маршруту
-                preg_match("/$requestString/", $requestUrl, $matches);
-
-                // Если совпадений нет, то возвращаем $this->isToken() == false
-                if (!isset($matches[0])) {
-                    return $this->isToken();
-                }
-            }
-
-            return $this->handleRequest($route, $realRequestString, $outRequestUrl, $params);
-        }
-    }
-
-    /**
-     * @param array $route
-     * @param array $requestArray
-     *
-     * @return array
-     */
-    protected function handlePattern(array $route, array $requestArray): array
-    {
-        $i                    = 0;
-        $params               = null;
-        $paramsKey            = [];
-        $matchesPattern       = [];
-        $completeRequestArray = [];
-
-        // Обходим элементы массива $pattern
-        foreach (explode('/', ltrim($route['pattern'], '/')) as $itemPattern) {
-            // Ищем совпадение строки запроса с шаблоном {...}
-            if (preg_match('/{[a-zA-Z0-9]+}/', $itemPattern, $matchesPattern) != 0) {
-
-                // Если есть элемент массива $i
-                if (isset($requestArray[$i])) {
-                    // Убираем {} из названия будующего ключа массива параметров
-                    preg_match('/[a-zA-Z0-9]+/', $matchesPattern[0], $paramsKey);
-                    // Присваиваем найденому параметру соответсвующий uri
-                    $params[$paramsKey[0]]  = $requestArray[$i];
-                    $completeRequestArray[] = $requestArray[$i];
-                }
-                // Если совпадений нет, то записываем данные не совпадающие
-                // с шаблоном в отдельный массив
-            } else {
-                $completeRequestArray[] = $itemPattern;
-            }
-
-            $i++;
-        }
-
-        return [$params, $completeRequestArray];
-    }
-
-    /**
-     * @param $completeRequestArray
-     *
-     * @return array
-     */
-    protected function handleCompleteRequestArray($completeRequestArray)
-    {
-        $requestString     = '';
-        $realRequestString = '';
-
-        if (count($completeRequestArray)) {
-            $requestString     = implode('\/', $completeRequestArray);
-            $realRequestString = implode('/', $completeRequestArray);
-        }
-
-        return [$requestString, $realRequestString];
-    }
-
-    /**
-     * @param array $route
-     * @param       $realRequestString
-     * @param       $outRequestUrl
-     * @param       $params
-     */
-    protected function handleRequest(array $route, $realRequestString, $outRequestUrl, $params)
-    {
-        // Если $realRequestString совпадает с 'REQUEST_URI'
-        if ($realRequestString == $outRequestUrl[0]) {
-            // Устанавливаем token true
-            $this->setToken(true);
-            $this->setCallable($route, $params);
-            (DEV === 'test') ?: exit(); // @codeCoverageIgnore
-        }
-    }
-
-    /**
-     * @param $requestUrl
-     *
-     * @return mixed
-     */
-    protected function getOutRequestUrl($requestUrl)
-    {
-        $outRequestUrl = [];
-
-        if (strpos($requestUrl, '?') !== false) {
-            preg_match('~[/[:word:]-]+(?=\?)~', $requestUrl, $outRequestUrl);
-
-            return $outRequestUrl;
-        }
-
-        $outRequestUrl[0] = $requestUrl;
-
-        return $outRequestUrl;
-    }
-
-    /**
-     * @param array $route
      * @param       $params
      *
      * @return mixed
      */
-    protected function setCallable(array $route, $params)
+    public function setCallable(array $route, $params)
     {
         // Если $route['method'] является экземпляром ксласса Closure
         // возвращаем замыкание
@@ -466,49 +346,6 @@ class Router //implements RouterInterface
      * @var RouterMatch
      */
     protected $routerMatch;
-
-    /**
-     * @param string|null $param
-     *
-     * @return mixed
-     */
-    protected function setRequestMethod(string $param = null)
-    {
-        if ($param === 'REST') {
-            switch ($this->container()->getPost('_method')) {
-                case 'PUT':
-                    $this->container()->setServer('REQUEST_METHOD', 'PUT');
-                    $route['http_method'] = 'PUT';
-                    $route['method']      = 'update';
-
-                    return $route;
-                case 'PATCH':
-                    $this->container()->setServer('REQUEST_METHOD', 'PATCH');
-                    $route['http_method'] = 'PATCH';
-                    $route['method']      = 'update';
-
-                    return $route;
-                case 'DELETE':
-                    $this->container()->setServer('REQUEST_METHOD', 'DELETE');
-                    $route['http_method'] = 'DELETE';
-                    $route['method']      = 'delete';
-
-                    return $route;
-            }
-        } else {
-            switch ($this->container()->getPost('_method')) {
-                case 'PUT':
-                    $this->container()->setServer('REQUEST_METHOD', 'PUT');
-                    break;
-                case 'PATCH':
-                    $this->container()->setServer('REQUEST_METHOD', 'PATCH');
-                    break;
-                case 'DELETE':
-                    $this->container()->setServer('REQUEST_METHOD', 'DELETE');
-                    break;
-            }
-        }
-    }
 
     /**
      * @return bool
