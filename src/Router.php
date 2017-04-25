e8e5f292958ff86a368b732de7d9897635403abc
Jagepard <dankorot@gmail.com>
2017-04-25 15:31:04 +0300

    Оставляет только Фассад и декораторы

--- src/Router.php
+++ src/Router.php
@@ -20,12 +20,13 @@ namespace Rudra;
  */
 class Router implements RouterInterface
 {
+
+    use SetContainerTrait;
+
     /**
      * @var RouterFacade
      */
     protected $routerFacade;
-
-    use SetContainerTrait;
 
     /**
      * Router constructor.
@@ -37,14 +38,14 @@ class Router implements RouterInterface
     public function __construct(ContainerInterface $container, string $namespace, string $templateEngine)
     {
         $this->container      = $container;
-        $this->namespace      = $namespace;
-        $this->templateEngine = $templateEngine;
         set_exception_handler([new RouterException(), 'handler']);
 
         $this->routerFacade = new RouterFacade(
             $this->container,
             new RequestMethod($this->container),
-            new MatchMethod($this->container, new MatchRequest($this->container, $this))
+            new MatchMethod($this->container, new MatchRequest($this->container, $this)),
+            $namespace,
+            $templateEngine
         );
     }
 
@@ -75,24 +76,15 @@ class Router implements RouterInterface
      */
     public function directCall(array $route, $params = null): void
     {
-        $controller = new $route['controller']($this->container());
+        $this->routerFacade()->directCallDecorator($route, $params);
+    }
 
-        if (method_exists($controller, $route['method'])) {
-            $method = $route['method'];
-        } else {
-            throw new RouterException('503');
-        }
-
-        // Инициализуруем
-        $controller->init($this->container(), $this->templateEngine());
-        // Выполняем методы before до основного вызова
-        $controller->before();
-        !isset($route['middleware']) ?: $this->handleMiddleware($route['middleware']);
-        // Собственно вызываем экшн, в зависимости от наличия параметров
-        isset($params) ? $controller->{$method}($params) : $controller->{$method}();
-        // Выполняем методы after
-        !isset($route['after_middleware']) ?: $this->handleMiddleware($route['after_middleware']);
-        $controller->after(); // after
+    /**
+     * @param array $middleware
+     */
+    public function handleMiddleware(array $middleware)
+    {
+        $this->routerFacade()->handleMiddleware($middleware);
     }
 
     /**
@@ -172,7 +164,7 @@ class Router implements RouterInterface
                 break;
             case 'POST':
                 if ($this->container()->hasPost('_method')) {
-                    $route = array_merge($route, $this->routerFacade()->requestMethod()->setRequestMethod('REST'));
+                    $route = array_merge($route, $this->routerFacade()->setRequestMethod('REST'));
                 } else {
                     $route['http_method'] = 'POST';
                     $route['method']      = 'create';
@@ -200,33 +192,8 @@ class Router implements RouterInterface
      */
     public function annotation(string $class, string $method, int $number = 0): void
     {
-        $controller = $this->setClassName($class, 'controllersNamespace');
-        $result     = $this->container()->get('annotation')->getMethodAnnotations($controller, $method);
-
-        if (isset($result['Routing'])) {
-            $http_method = $result['Routing'][$number]['method'] ?? 'GET';
-            $dataRoute   = $this->setRouteData($class, $method, $number, $result, $http_method);
-
-            $this->set($dataRoute);
-        }
+        $this->routerFacade()->annotationDecorator($class, $method, $number);
     }
-
-    /**
-     * @param array $middleware
-     */
-    public function handleMiddleware(array $middleware)
-    {
-        $current = array_shift($middleware);
-
-        if (is_array($current)) {
-            $currentMiddleware = $this->setClassName($current[0], 'middlewareNamespace');
-
-            (new $currentMiddleware($this->container()))($current, $middleware);
-        }
-    }
-
-    //////////////////////////////////////////////
-
 
     /**
      * @param array $route
@@ -236,154 +203,14 @@ class Router implements RouterInterface
      */
     public function setCallable(array $route, $params)
     {
-        // Если $route['method'] является экземпляром ксласса Closure
-        // возвращаем замыкание
-        if ($route['method'] instanceof \Closure) {
-            return $route['method']();
-        }
-
-        $route['controller'] = $this->setClassName($route['controller'], 'controllersNamespace');
-        isset($params) ? $this->directCall($route, $params) : $this->directCall($route);
+        $this->routerFacade()->setCallableDecorator($route, $params);
     }
-
-
-    /**
-     * @param string $className
-     * @param string $type
-     *
-     * @return string
-     * @throws RouterException
-     */
-    protected function setClassName(string $className, string $type): string
-    {
-        if (strpos($className, '::namespace') !== false) {
-            $classNameArray = explode('::', $className);
-
-            if (class_exists($classNameArray[0])) {
-                $className = $classNameArray[0];
-            } else {
-                throw new RouterException('503');
-            }
-        } else {
-
-            if (class_exists($this->$type() . $className)) {
-                $className = $this->$type() . $className;
-            } else {
-                throw new RouterException('503');
-            }
-        }
-
-        return $className;
-    }
-
-    /**
-     * @param string $class
-     * @param string $method
-     * @param int    $number
-     * @param        $result
-     * @param        $http_method
-     *
-     * @return array
-     */
-    protected function setRouteData(string $class, string $method, int $number, $result, $http_method)
-    {
-        $dataRoute = ['pattern'     => $result['Routing'][$number]['url'],
-                      'controller'  => $class,
-                      'method'      => $method,
-                      'http_method' => $http_method
-        ];
-
-        if (isset($result['Middleware'])) {
-            $dataRoute = array_merge($dataRoute, ['middleware' => $this->handleAnnotationMiddleware($result['Middleware'])]);
-        }
-
-        if (isset($result['AfterMiddleware'])) {
-            $dataRoute = array_merge($dataRoute, ['after_middleware' => $this->handleAnnotationMiddleware($result['AfterMiddleware'])]);
-        }
-
-        return $dataRoute;
-    }
-
-
-    /**
-     * @param array $annotation
-     *
-     * @return array
-     */
-    protected function handleAnnotationMiddleware(array $annotation): array
-    {
-        $i          = 0;
-        $middleware = [];
-
-        foreach ($annotation as $item) {
-            $middleware[$i][] = $item['name'];
-
-            if (isset($item['params'])) {
-                $middleware[$i][] = $item['params'];
-            }
-            $i++;
-        }
-
-        return $middleware;
-    }
-
-    /**
-     * @var bool
-     */
-    protected $token = false;
-
-    /**
-     * @var string
-     */
-    protected $namespace;
-
-    /**
-     * @var string
-     */
-    protected $templateEngine;
-
-    /**
-     * @var RouterMatch
-     */
-    protected $routerMatch;
 
     /**
      * @return bool
      */
     public function isToken(): bool
     {
-        return $this->token;
-    }
-
-    /**
-     * @param bool $token
-     */
-    public function setToken(bool $token)
-    {
-        $this->token = $token;
-    }
-
-    /**
-     * @return mixed
-     */
-    protected function controllersNamespace()
-    {
-        return $this->namespace . 'Controllers\\';
-    }
-
-    /**
-     * @return mixed
-     */
-    protected function middlewareNamespace()
-    {
-        return $this->namespace . 'Middleware\\';
-    }
-
-    /**
-     * @return mixed
-     */
-    protected function templateEngine()
-    {
-        return $this->templateEngine;
+        return $this->routerFacade()->matchMethod()->matchRequest()->isToken();
     }
 }
