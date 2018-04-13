fa74d8bd8c27ad8a9eb94e56bfc526ff273ae947
Jagepard <dankorot@gmail.com>
2018-04-13 15:46:55 +0300

    Убрал лишнее

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -3,7 +3,3 @@
 define('DEV', 'test');
 
 require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
-require_once dirname(__DIR__) . '/tests/stub/route.php';
-require_once dirname(__DIR__) . '/tests/stub/MainController.php';
-require_once dirname(__DIR__) . '/tests/stub/Middleware.php';
-require_once dirname(__DIR__) . '/Router.php';
