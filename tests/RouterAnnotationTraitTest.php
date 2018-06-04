e0711848ae6849facb742ac99c02b655d06eebc7
Jagepard <dankorot@gmail.com>
2018-06-04 15:14:45 +0300

    annotation Update

--- tests/RouterAnnotationTraitTest.php
+++ tests/RouterAnnotationTraitTest.php
@@ -29,7 +29,7 @@ class RouterAnnotationTraitTest extends PHPUnit_Framework_TestCase
         Container::$app  = null;
         $this->container = Container::app();
         $this->container->setBinding(ContainerInterface::class, Container::$app);
-        $this->container->set('annotation', 'Rudra\Annotations');
+        $this->container->set('annotation', 'Rudra\Annotation');
         $this->container->set('router', 'Rudra\Router', ['namespace' => 'Rudra\\Tests\\Stub\\', 'templateEngine' => ['engine' => 'twig']]);
     }
 
--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -31,7 +31,7 @@ class RouterTest extends PHPUnit_Framework_TestCase
         Container::$app  = null;
         $this->container = Container::app();
         $this->container->setBinding(ContainerInterface::class, Container::$app);
-        $this->container->set('annotation', 'Rudra\Annotations');
+        $this->container->set('annotation', 'Rudra\Annotation');
         $this->container->set('router', 'Rudra\Router', ['namespace' => 'Rudra\\Tests\\Stub\\']);
     }
 
