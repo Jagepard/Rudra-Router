2c57bcb6aeaf3efddc61c8971e0abba34090a7fe
Jagepard <dankorot@gmail.com>
2020-10-27 17:19:47 +0300

    add Annotation

--- src/Traits/RouterAnnotationTrait.php
+++ src/Traits/RouterAnnotationTrait.php
@@ -9,6 +9,8 @@ declare(strict_types=1);
 
 namespace Rudra\Router\Traits;
 
+use Rudra\Annotation\Annotation;
+
 trait RouterAnnotationTrait
 {
     public function annotationCollector(array $data, bool $multilevel = false)
@@ -33,7 +35,7 @@ trait RouterAnnotationTrait
     public function annotation(string $controller, string $action = 'actionIndex', int $number = 0): void
     {
         $className  = $this->setClassName($controller, $this->namespace . "Controllers\\");
-        $annotation = $this->rudra()->get("annotation")->getAnnotations($className, $action);
+        $annotation = $this->rudra()->get(Annotation::class)->getAnnotations($className, $action);
 
         if (isset($annotation["Routing"])) {
             $httpMethod = $annotation["Routing"][$number]["method"] ?? "GET";
