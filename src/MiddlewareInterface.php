3fc1b680109720814106f19fb79d6d59907221de
Jagepard <Jagepard@yandex.ru>
2022-02-08 09:32:55 +0300

    add MiddlewareInterface

---
+++ src/MiddlewareInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author    : Jagepard <jagepard@yandex.ru">
+ * @license   https://mit-license.org/ MIT
+ */
+
+namespace Rudra\Router;
+
+interface MiddlewareInterface
+{
+    /**
+     * @param  array $chainOfMiddlewares
+     * @return void
+     */
+    public function next(array $chainOfMiddlewares): void;
+}
